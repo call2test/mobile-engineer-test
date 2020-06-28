1063e85a13ce7e54c78da9797d80215e09973a50
matthewFisher <m.r.f.48215@gmail.com>
2020-06-28 15:46:36 +0900

    Adding typescript and styling packages, setting up config files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    'module:react-native-dotenv',
+  ],
+  plugins: ['module:babel-plugin-styled-components'],
 };
